3027ce67a159e0fa5896237cf8a752ce3e9abc72
amandaratio <amandaratio@gmail.com>
2017-10-18 10:10:54 -0700

    homepage basic unstyled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public bookTitle: string;
+  public bookAuthor: string;
+  public bookSubject: string;
+  showHide: false;
+  public books:Array<Object> =[
+    {title:"Sample1", author:"Author1", subject:"", cover:""},
+    {title:"Sample2", author:"Author2", subject:"", cover:""},
+    {title:"Sample3", author:"Author3", subject:"", cover:""},
+    {title:"Sample4", author:"Author4", subject:"", cover:""},
+    {title:"Sample5", author:"Author5", subject:"", cover:""},
+    {title:"Sample6", author:"Author6", subject:"", cover:""}
+  ];
+
+  title = 'Digital Book Shelf';
+
+  addBook(): void {
+    var newBook = {
+      title:"",
+      author:"",
+      subject:""
+    }
+    newBook.title = this.bookTitle;
+    newBook.author = this.bookAuthor;
+    newBook.subject = this.bookSubject;
+
+    this.books.push(newBook);
+
+  }
+
+
+
 }
