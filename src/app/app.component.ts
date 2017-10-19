23413e910ab82ebb66cf08997d4adc0529e5f97e
amandaratio <amandaratio@gmail.com>
2017-10-19 13:11:50 -0700

    injectable for books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,38 @@
 import { Component } from '@angular/core';
 
+//get books 
+import { Books } from './books.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [Books]
 })
+
 export class AppComponent {
   public bookTitle: string;
   public bookAuthor: string;
   public bookSubject: string;
   showHide: false;
-  public books:Array<Object> =[
-    {title:"Sample1", author:"Author1", subject:"", cover:""},
-    {title:"Sample2", author:"Author2", subject:"", cover:""},
-    {title:"Sample3", author:"Author3", subject:"", cover:""},
-    {title:"Sample4", author:"Author4", subject:"", cover:""},
-    {title:"Sample5", author:"Author5", subject:"", cover:""},
-    {title:"Sample6", author:"Author6", subject:"", cover:""}
-  ];
-
+  public library:Array<Object>;
+  
   title = 'Digital Book Shelf';
 
-  addBook(): void {
+  constructor(_books: Books) {
+    this.library = _books.getLib();
+  }
+
+  add(title, author, sub) {
     var newBook = {
       title:"",
       author:"",
       subject:""
     }
-    newBook.title = this.bookTitle;
-    newBook.author = this.bookAuthor;
-    newBook.subject = this.bookSubject;
-
-    this.books.push(newBook);
-
+    newBook.title = title;
+    newBook.author = author;
+    this.library.push(newBook);
   }
 
-
-
 }
+
